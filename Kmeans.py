68318ae0025ab67f094a7eb6a97486d992d5c57f
Michael Khalil <mikegeorge2012@gmail.com>
2020-03-06 01:54:39 +0200

    add confusion matrix functions. Add initial cluster means function. Do some fixes.

--- Kmeans.py
+++ Kmeans.py
@@ -6,9 +6,12 @@ import sys
 from scipy.io import loadmat
 import random
 import synthetic
+from sklearn.cluster import KMeans
+import confusion
+
 np.set_printoptions(threshold=sys.maxsize)
 
-def kmeans(Data,K,C):
+def kmeansX(Data,K,C):
     #Data array, k classes, C means for classes
     #the following step to know the dimensions of the data
     dimSize=len(Data.shape)
@@ -49,6 +52,8 @@ def kmeans(Data,K,C):
             else:
                 newClusterMeans[x]=np.zeros(newClusterMeans[x].size)    
         counter+=1
+        # print(newClusterMeans)
+        # print(clusterSum)
        
     finalClusteredImage=np.zeros(Data.shape)
 
@@ -58,7 +63,37 @@ def kmeans(Data,K,C):
             finalClusteredImage[y][p]=newClusterMeans[(pixelClusterNo[y][p]).astype(int)]
     #the finalclassImage is the clustered image each class has its mean color
     #pixelclusterno is an image with each pixel representing its class number mostly used in the hyper spectral   
-    return finalClusteredImage , pixelClusterNo 
+    return finalClusteredImage , pixelClusterNo , newClusterMeans
+
+
+def choose_initial_cluster_centers(points, clusters, dim):
+    points = np.reshape(points,((42*43),points.shape[2]))
+    us = np.zeros((clusters,dim))         # centers of each cluster
+    indicies = np.zeros(clusters)
+
+    index = np.copy(np.random.choice(points.shape[0], 1)[0])         # choose first point randomly
+    indicies[0] = np.copy(index)
+    us[0] =  np.copy(points[index])
+
+    for i in range(1,clusters):
+        max_distance = 0
+        for j in range(points.shape[0]):    # choose the center with the biggest distancce form last center
+            if j not in indicies:           # check if the the point is already a center to cluster
+                point = np.copy(points[j])
+                if point not in us:
+                    last_point = np.copy(us[i-1])
+                    distance = np.linalg.norm(point-last_point)     # Eucledian distance
+
+                    if distance > max_distance:
+                        max_distance = distance
+                        index = j
+                        u = np.copy(point)
+        
+        
+        indicies[i] = np.copy(index)
+        us[i] = np.copy(u)
+
+    return us
 
 
 #genrates initial means randomly
@@ -83,30 +118,33 @@ line2_range = int(width*.05)
 sigma = int(num_color_range / 6)
 noise_probability = 0.9
 
-img1 = synthetic.syn_quarter_image(height,width,num_color_range,0,0)
-img5 = synthetic.syn_image_lines(height,width,num_color_range,line1_start,line1_range,line2_start,line2_range,0,0)
-img1_testGrayImage_hi = synthetic.syn_quarter_image(height,width,num_color_range,sigma,noise_probability)
-img5_testGrayImage_hi = synthetic.syn_image_lines(height,width,num_color_range,line1_start,line1_range,line2_start,line2_range,sigma,noise_probability)
+# img1 = synthetic.syn_quarter_image(height,width,num_color_range,0,0)
+# img5 = synthetic.syn_image_lines(height,width,num_color_range,line1_start,line1_range,line2_start,line2_range,0,0)
+# img1_testGrayImage_hi = synthetic.syn_quarter_image(height,width,num_color_range,sigma,noise_probability)
+# img5_testGrayImage_hi = synthetic.syn_image_lines(height,width,num_color_range,line1_start,line1_range,line2_start,line2_range,sigma,noise_probability)
 
-plt.imsave("img1.png", img1, cmap='gray', vmin=0, vmax=255)
-plt.imsave("img5.png", img5, cmap='gray', vmin=0, vmax=255)
-plt.imsave("img1_testGrayImage_hi.png", img1_testGrayImage_hi, cmap='gray', vmin=0, vmax=255)
-plt.imsave("img5_testGrayImage_hi.png", img5_testGrayImage_hi, cmap='gray', vmin=0, vmax=255)
+# plt.imsave("img1.png", img1, cmap='gray', vmin=0, vmax=255)
+# plt.imsave("img5.png", img5, cmap='gray', vmin=0, vmax=255)
+# plt.imsave("img1_testGrayImage_hi.png", img1_testGrayImage_hi, cmap='gray', vmin=0, vmax=255)
+# plt.imsave("img5_testGrayImage_hi.png", img5_testGrayImage_hi, cmap='gray', vmin=0, vmax=255)
 ######
 
 #for rgb
 # img = plt.imread("res/star.jpg", format="jpg")
+# print(img.shape)
 # arr=randomClassMeanGenrator(img,4,3)
-# kmImage,clusterNoImage=kmeans(img,4,arr)
+# kmImage,clusterNoImage,newClusterMeans=kmeans(img,4,arr)
 # plt.imshow(kmImage.astype(np.uint8))
 # plt.savefig("outputrgb.png")
 
 #for gray
-#img = plt.imread("res/starg.jpg", format="jpg")
-#arrg=randomClassMeanGenrator(img,2,1)
-# kmImage,clusterNoImage=kmeans(img,2,arrg)
+# img = plt.imread("res/starg.jpg", format="jpg")
+# print(img.shape)
+# arrg=randomClassMeanGenrator(img,2,1)
+# kmImage,clusterNoImage,newClusterMeans=kmeans(img,2,arrg)
 # plt.imshow(kmImage.astype(np.uint8),cmap="gray")
 # plt.savefig("outputgray.png")
+
 
 #for spectral
 img= loadmat('res/SalinasA_Q3.mat')
@@ -114,14 +152,78 @@ groundtruth=loadmat('res/SalinasA_GT3.mat')
 imgdata = img['Q3']
 #print(imgdata)
 
+
+imgdata = (imgdata / np.max(imgdata)) * 255
+imgdatak = np.reshape(imgdata, (imgdata.shape[0] * imgdata.shape[1], imgdata.shape[2]))
+
+kmeans = KMeans(n_clusters=5)
+kmeans.fit(imgdatak)
+cl=kmeans.cluster_centers_
+
 groundtruthdata=groundtruth['Q3_GT']
 #[  0 182 219 237 255] it contains 5 unique classes not 7
-#print(np.unique(groundtruthdata))
-arrspec=randomClassMeanGenrator(imgdata,5,imgdata.shape[2])
-print(arrspec)
-kmImage,clusterNoImage=kmeans(imgdata,5,arrspec)
+print(groundtruthdata[5])
+arrspec =np.zeros((5,204))
+arrspec[0]=cl[0]
+arrspec[1]=cl[1]
+arrspec[2]=cl[2]
+arrspec[3]=cl[3]
+arrspec[4]=cl[4]
+# arrspec=randomClassMeanGenrator(imgdata,5,imgdata.shape[2])
+arrspec=choose_initial_cluster_centers(imgdata,5,imgdata.shape[2])
+# print(arrspec)
 
+# for i in range(arrspec.shape[0]):
+#     for j in range(i+1,arrspec.shape[0]):
+#         print(str(i) + " " + str(j))
+#         print((arrspec[j] == arrspec[i]).all())
+
+kmImage,clusterNoImage,newClusterMeans= kmeansX(imgdata,5,arrspec)
+reIndexed=np.zeros(clusterNoImage.shape)
+norms=np.zeros(5)
+for i in range(5):
+    norms[i]=np.linalg.norm(newClusterMeans[i]) 
+normssorted=np.unique(norms)
+uniqueTruthvalues=np.unique(groundtruthdata)    
+for j in range(0,clusterNoImage.shape[0]):
+    for k in range(0,clusterNoImage.shape[1]):
+        classNumber=clusterNoImage[j][k]
+        classMeanNorm=norms[(classNumber).astype(int)]
+        toBeAddedClassNo=uniqueTruthvalues[np.where(normssorted==classMeanNorm)]
+        reIndexed[j][k]=toBeAddedClassNo
+print(reIndexed[5])
+# print(clusterNoImage[0])
+# print(groundtruthdata[0])
 # print(kmImage.shape)
 # print(imgdata.shape[2])
 
+plt.figure()
+plt.imshow(clusterNoImage.astype(np.uint8))
+plt.savefig("number.png")
 
+plt.imshow(reIndexed.astype(np.uint8))
+plt.savefig("number4.png")
+
+# plt.figure()
+# plt.imshow(groundtruthdata.astype(np.uint8))
+# plt.savefig("number2.png")
+
+
+# from sklearn.cluster import KMeans
+
+# plt.scatter(imgdata[:,0],imgdata[:,1], label='True Position')
+
+# print(img['Q3'])
+
+# kmeans = KMeans(n_clusters=5)
+# kmeans.fit(imgdata)
+# print(kmeans.cluster_centers_)
+# print(kmeans.labels_)
+
+plt.figure()
+plt.imshow(np.reshape(kmeans.labels_,(42,43)).astype(np.uint8))
+plt.savefig("number3.png")
+
+
+cm = confusion.confusion_matrix_compute(reIndexed,groundtruthdata)
+confusion.confusion_matrix_save(cm)
---
+++ confusion.py
@@ -0,0 +1,36 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def confusion_matrix_compute(predicts, labels):
+    
+    predicts_flat = predicts.flatten()
+    labels_flat = labels.flatten()
+    num_classes = 5
+
+    cm = np.zeros((num_classes,num_classes))
+
+    for i in range(predicts_flat.shape[0]):
+        predict = int(predicts_flat[i])
+        label = int(labels_flat[i])
+        cm[label:label+1, predict:predict+1] += 1
+
+    return cm
+
+def confusion_matrix_save(cm, path=''):
+    plt.figure()
+    plt.imshow(cm)
+    plt.title("Confusion Matrix")
+    plt.colorbar()
+    plt.tight_layout()
+
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[0]):
+            plt.text(j, i, int(cm[i, j]), ha="center", va="center", color="w")
+
+    plt.savefig(path + 'Confusion.png')
+
+# cm = np.array([[0,1], [2,3]])
+# l = np.array([[0,1], [2,3]])
+# r = confusion_matrix_compute(cm,l)
+# confusion_matrix_save(r)
